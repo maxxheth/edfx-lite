b2b5f6d3d479a6ed50bdd344e257d154146f603c
maxx <maxx@edfx.webdev>
2020-01-20 20:41:19 -0600

    Swapped out formats on service page

---
+++ views/services-copy.php
@@ -0,0 +1,394 @@
+<?php
+
+error_reporting(-1);
+ini_set('display_errors', 1);
+
+// require __DIR__ . '/templates/header.php';
+// require __DIR__ . '/templates/footer.php';
+
+?>
+
+<?php get_header(); ?>
+
+<div id="top-container"></div>
+
+<div class="card-container">
+
+    <!--BEGIN 1ST SECTION-->
+
+    <section id="home-section-01" class="home-section home-section-01">
+
+        <div id="red-card-01" class="one-half card card--red-bg">
+
+            <h2 class="card-title" id="web-des-dev-title">Best-In-Class Web Design & Development</h2>
+
+        </div>
+
+        <div id="blue-card-01" class="one-half card--blue-bg card" data-simplebar>
+
+            <p>We specialize in custom web design and development of websites that are unique, fast, and tailored to our
+            clients’ business needs and strategies.</p>
+
+            <p>Why do you need a website? Nobody really asks this question today because the answer seems obvious, but
+                it’s important to remember who your website is actually for: your customers!</p>
+
+            <p>When you have people visit your website, it’s important for them to have a great user experience? What
+                does that mean? It means that your website:</p>
+
+            <ul>
+                <li>Needs to make a good first impression.</li>
+
+                <li>Needs to get to the point quickly about who you are and what you do.</li>
+
+                <li>Needs to be blazing fast on both desktop and mobile devices. People hate slow websites, and this
+                    probably includes you!</li>
+            </ul>
+
+            <p>Where do we come in? We build websites and digital products that make excellent first impressions and
+                stand the test of time.</p>
+
+
+            <button class="edfx-ripple edfx-ripple--red"><a href="#">Learn More</a></button>
+
+        </div>
+
+        
+
+        <div id="mobile-card-01" class="mobile-card">
+
+            <div class="mobile-card__side mobile-card__side--red">
+                <h2>First-Class Web Designers &amp; Developers</h2>
+
+                <h3 class="tap-me-prompt">Tap Me!</h3>
+
+            </div>
+
+            <div class="mobile-card__side mobile-card__side--blue">
+
+                <p>Web design is more than just what your website looks like. It’s about how visitors interact with your site and how they consume the information they need to make a decision to do business with you.</p>
+
+                <p>The kind of impact you can make with a solid design can be powerful.</p>
+
+                <p>Web design also involves looking at trends. What are the biggest companies in your space doing? What are your competitors doing? It’s not just the artsy veneer that we’re looking at; it’s what they’re doing to turn their visitors into customers. That’s what we’re after.</p>
+
+                <p>It’s no secret that looking the part is half the battle in any meaningful business transaction we engage in, and that’s no different when it comes to your website.</p>
+
+
+            </div>
+
+        </div>
+
+    </section>
+
+
+    <!--BG IMG ARRAY-->
+
+
+    <!--JARALLAX SCENE 01-->
+
+
+    <div id="jarallax-scene-01" class="jarallax"
+        style="background-image: url(/images/people-meeting-workspace-team-1.jpg)">
+        <!-- <h2 class="jarallax-title">We Get Results</h2> -->
+    </div>
+
+
+    <!--END 1ST SECTION-->
+
+    <!--BEGIN 2ND SECTION-->
+
+    <section id="home-section-02" class="home-section home-section-02">
+
+        <div id="red-card-02" class="one-half card card--red-bg">
+
+            <h2 class="card-title">Web Design</h2>
+
+        </div>
+
+        <div id="blue-card-02" class="one-half card--blue-bg card" data-simplebar>
+
+            <p>Web design is more than just what your website looks like. It’s about how visitors interact with your site and how they consume the information they need to make a decision to do business with you.</p>
+
+            <p>The kind of impact you can make with a solid design can be powerful.</p>
+
+            <p>Web design also involves looking at trends. What are the biggest companies in your space doing? What are your competitors doing? It’s not just the artsy veneer that we’re looking at; it’s what they’re doing to turn their visitors into customers. That’s what we’re after.</p>
+
+            <p>It’s no secret that looking the part is half the battle in any meaningful business transaction we engage in, and that’s no different when it comes to your website.</p>
+
+            <button class="edfx-ripple edfx-ripple--red"><a href="#">Learn More</a></button>
+
+        </div>
+
+        <div id="mobile-card-02" class="mobile-card">
+
+            <div class="mobile-card__side mobile-card__side--red">
+                <h2>First-Class Web Designers &amp; Developers</h2>
+
+                <h3 class="tap-me-prompt">Tap Me!</h3>
+
+            </div>
+
+            <div class="mobile-card__side mobile-card__side--blue">
+
+                <p>Web design is more than just what your website looks like. It’s about how visitors interact with your site and how they consume the information they need to make a decision to do business with you.</p>
+
+                <p>The kind of impact you can make with a solid design can be powerful.</p>
+
+                <p>Web design also involves looking at trends. What are the biggest companies in your space doing? What are your competitors doing? It’s not just the artsy veneer that we’re looking at; it’s what they’re doing to turn their visitors into customers. That’s what we’re after.</p>
+
+                <p>It’s no secret that looking the part is half the battle in any meaningful business transaction we engage in, and that’s no different when it comes to your website.</p>
+
+
+            </div>
+
+        </div>
+
+    </section>
+
+    <!--JARALLAX SCENE 02-->
+
+
+    <div id="jarallax-scene-02" class="jarallax"
+        style="background-image: url( /images/discussing-plans-for-design-1.jpg )"></div>
+
+
+    <!--END 2ND SECTION-->
+
+    <!--BEGIN 3RD SECTION-->
+
+    <section id="home-section-03" class="home-section home-section-03">
+
+        <div id="red-card-03" class="one-half card card--red-bg">
+
+            <h2 class="card-title">Web Development</h2>
+
+        </div>
+
+        <div id="blue-card-03" class="one-half card--blue-bg card" data-simplebar>
+			
+            <p>The ever faithful companion to web design is web development. It’s akin to the V12 engine in a Ferrari, the invisible moving parts behind the scenes that make the magic happen on the big screen.</p>
+            
+            <p>While web design is concerned with what your site looks like and how users interact with it, web development is concerned with making things work.</p>
+            
+            <p>We provide development services for a variety of common use-case applications, such as ecommerce, payment forms and services, and interactive forms.</p>
+
+        </div>
+
+        
+
+        <div id="mobile-card-03" class="mobile-card">
+
+            <div class="mobile-card__side mobile-card__side--red">
+                <h2>Prototyping / Design Phase</h2>
+
+                <h3 class="tap-me-prompt">Tap Me!</h3>
+
+            </div>
+
+            <div class="mobile-card__side mobile-card__side--blue">
+                <p>During the first phase, we create a live mockup site using raw HTML &amp; CSS that you can visit in
+                    real time and
+                    interact with. And if you already have a design in PSD format, we can gladly convert that into an
+                    HTML &amp; CSS
+                    site.</p>
+
+                <p>And if you have no idea what HTML &amp; CSS are, just think of HTML as the building blocks for a site
+                    and CSS as the
+                    paint, and you\'ll get the picture.</p>
+
+            </div>
+
+        </div>
+
+    </section>
+
+    <!--JARALLAX SCENE 03-->
+
+
+    <div id="jarallax-scene-03" class="jarallax" style="background-image: url(/images/web-design-team-working.jpg )">
+    </div>
+
+
+    <!--END 3RD SECTION-->
+
+    <!--BEGIN 4TH SECTION-->
+
+    <section id="home-section-04" class="home-section home-section-04">
+
+        <canvas class="home-section-canvas"></canvas>
+
+        <div id="red-card-04" class="one-half card card--red-bg">
+
+            <h2 class="card-title" id="dev-phase">Development Phase</h2>
+
+        </div>
+
+        <div id="blue-card-04" class="one-half card card--blue-bg">
+
+            <p>After the design is completely finished to your satisfaction, we will work on implementing any advanced
+                features
+                that you requested during the first phase.</p>
+
+            <p>Ultimately, the goal is to create a site that you yourself would gladly use if you were one of your
+                customers.
+                Why have it any other way?</p>
+
+        </div>
+
+        
+
+        <div id="mobile-card-04" class="mobile-card">
+
+            <div class="mobile-card__side mobile-card__side--red">
+                <h2>Development Phase</h2>
+
+                <h3 class="tap-me-prompt">Tap Me!</h3>
+
+            </div>
+
+            <div class="mobile-card__side mobile-card__side--blue">
+                <p>After the design is completely finished to your satisfaction, we will work on implementing any
+                    advanced features
+                    that you requested during the first phase.</p>
+
+                <p>Ultimately, the goal is to create a site that you yourself would gladly use if you were one of your
+                    customers.
+                    Why have it any other way?</p>
+
+
+            </div>
+
+        </div>
+
+    </section>
+
+    <!--JARALLAX SCENE 04-->
+
+
+    <div id="jarallax-scene-04" class="jarallax"
+        style="background-image: url(/images/maik-jonietz-535261-unsplash-1024x683.jpg )"></div>
+
+
+    <!--END 4TH SECTION-->
+
+    <!--BEGIN 5TH SECTION-->
+
+    <!-- <section id="home-section-05" class="home-section home-section-05">
+
+        <div id="blue-card-05" class="one-half card card--blue-bg">
+
+            <p>After development is completed, we meet with you once again to review the site in full and also conduct
+                thorough
+                tests on all major desktop and mobile browsers to ensure that your site is working perfectly.</p>
+
+            <p>At this stage, we\'ll also invite you to test every feature out for yourself to make sure they\'re all
+                working
+                perfectly.</p>
+
+        </div>
+
+        <div id="red-card-05" class="one-half card card--red-bg">
+
+            <h2 class="card-title" id="testing-phase-title">Testing Phase</h2>
+
+        </div>
+
+        <div id="mobile-card-05" class="mobile-card">
+
+            <div class="mobile-card__side mobile-card__side--red">
+                <h2>Testing Phase</h2>
+
+                <h3 class="tap-me-prompt">Tap Me!</h3>
+
+            </div>
+
+            <div class="mobile-card__side mobile-card__side--blue">
+                <p>After development is completed, we meet with you once again to review the site in full and also
+                    conduct thorough
+                    tests on all major desktop and mobile browsers to ensure that your site is working perfectly.</p>
+
+                <p>At this stage, we\'ll also invite you to test every feature out for yourself to make sure they\'re
+                    all working
+                    perfectly.</p>
+
+            </div>
+
+        </div>
+
+    </section> -->
+
+    <!--JARALLAX SCENE 05-->
+
+    <!-- <div id="jarallax-scene-05" class="jarallax"
+        style="background-image: url(/images/markus-spiske-207946-unsplash-1024x683.jpg )"></div> -->
+
+
+    <!--END 5TH SECTION-->
+
+    <!--BEGIN 6TH SECTION-->
+
+    <!-- <section id="home-section-06" class="home-section home-section-06">
+
+        <div id="blue-card-06" class="one-half card card--blue-bg">
+
+            <p>Once testing is fully completed and all development has been thoroughly accounted for, now it\'s time to
+                launch
+                the site!</p>
+
+            <p>If you already have your own hosting service, then you\'ll be good to go, but if you don\'t really want
+                to
+                deal
+                with the hassle of setting up your own hosting server, we\'ll gladly take care of that for you under our
+                one
+                of our
+                monthly hosting and maintenance plans!</p>
+
+        </div>
+
+        <div id="red-card-06" class="one-half card card--red-bg">
+
+            <h2 class="card-title">Time To Launch The Site!</h2>
+
+        </div>
+
+        <div id="mobile-card-06" class="mobile-card">
+
+            <div class="mobile-card__side mobile-card__side--red">
+                <h2>Time To Launch The Site!</h2>
+
+                <h3 class="tap-me-prompt">Tap Me!</h3>
+
+            </div>
+
+            <div class="mobile-card__side mobile-card__side--blue">
+                <p>Once testing is fully completed and all development has been thoroughly accounted for, now it\'s time
+                    to launch
+                    the site!</p>
+
+                <p>If you already have your own hosting service, then you\'ll be good to go, but if you don\'t really
+                    want to deal
+                    with the hassle of setting up your own hosting server, we\'ll gladly take care of that for you under
+                    our one of
+                    our monthly hosting and maintenance plans!</p>
+
+
+            </div>
+
+        </div>
+
+    </section> -->
+
+    <!--JARALLAX SCENE 06-->
+
+
+    <!-- <div id="jarallax-scene-06" class="jarallax"
+        style="background-image: url(/images/clement-h-544786-unsplash-1024x683.jpg)"></div> -->
+
+
+    <!--END 6TH SECTION-->
+
+
+</div>
+
+
+<?php get_footer('services'); ?>
