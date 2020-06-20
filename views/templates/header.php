21efbdfc82f4efa919c97a83eaa9e0c130de0ce3
Maximillian Heth <maxxheth@protonmail.com>
2020-06-20 18:49:39 -0500

    added google meta tag

--- views/templates/header.php
+++ views/templates/header.php
@@ -18,10 +18,7 @@ require dirname(__DIR__, 2) . '/helper_funcs/seo-data-parser.php';
 if (!function_exists('get_header')) {
     function get_header()
     {
-        global $root_url, $request;
-
-
-?>
+        global $root_url, $request; ?>
 
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
@@ -34,9 +31,10 @@ if (!function_exists('get_header')) {
 
 
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <?php
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="google-site-verification" content="eInQPjjJ-0W0QO2gpbkvCmx5ERh8h6VWcHesg28TSlg">
+  <?php
 
         SEOToolBox::insertPageSlugs([
 
@@ -57,135 +55,137 @@ if (!function_exists('get_header')) {
     
         ]);
     
-        SEOToolBox::SEOParser(dirname(__DIR__, 2) . '/seo-data.json');
-    
-    ?>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        SEOToolBox::SEOParser(dirname(__DIR__, 2) . '/seo-data.json'); ?>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="preload" href="/assets/css/dist/bundle.css" as="style">
+  <link rel="preload" href="/assets/css/dist/bundle.css" as="style">
 
-    <link rel="prefetch" href="/assets/css/dist/bundle.css">
+  <link rel="prefetch" href="/assets/css/dist/bundle.css">
 
-    <?php switch($_SERVER['REQUEST_URI']) : case '/' : ?>
+  <?php switch ($_SERVER['REQUEST_URI']) : case '/': ?>
 
-            <link rel="preload" href="/assets/js/dist/home.bundle.js" as="script">
+  <link rel="preload" href="/assets/js/dist/home.bundle.js" as="script">
 
-            <link rel="prefetch" href="/assets/js/dist/about.bundle.js">
-            
-            <link rel="next" href="/about">
+  <link rel="prefetch" href="/assets/js/dist/about.bundle.js">
 
-            <link rel="prefetch" href="/assets/js/dist/portfolio.bundle.js">
+  <link rel="next" href="/about">
 
-            <link rel="next" href="/portfolio">
-            
-            <link rel="prefetch" href="/assets/js/dist/services.bundle.js">
+  <link rel="prefetch" href="/assets/js/dist/portfolio.bundle.js">
 
-            <link rel="next" href="/services">
+  <link rel="next" href="/portfolio">
 
-        <?php break; case '/about' : ?>
-        
-            <link rel="preload" href="/assets/js/dist/about.bundle.js" as="script">
+  <link rel="prefetch" href="/assets/js/dist/services.bundle.js">
 
-            <link rel="prefetch" href="/assets/js/dist/home.bundle.js">
-        
-            <link rel="next" href="/">
+  <link rel="next" href="/services">
 
-            <link rel="prefetch" href="/assets/js/dist/portfolio.bundle.js">
+  <?php break;
+        case '/about': ?>
 
-            <link rel="next" href="/portfolio">
-        
-            <link rel="prefetch" href="/assets/js/dist/services.bundle.js">
+  <link rel="preload" href="/assets/js/dist/about.bundle.js" as="script">
 
-            <link rel="next" href="/services">
+  <link rel="prefetch" href="/assets/js/dist/home.bundle.js">
 
-        <?php break; case '/portfolio' : ?>
-        
-            <link rel="preload" href="/assets/js/dist/portfolio.bundle.js" as="script">
+  <link rel="next" href="/">
 
-            <link rel="prefetch" href="/assets/js/dist/about.bundle.js">
+  <link rel="prefetch" href="/assets/js/dist/portfolio.bundle.js">
 
-            <link rel="next" href="/about">
+  <link rel="next" href="/portfolio">
 
-            <link rel="prefetch" href="/assets/js/dist/home.bundle.js">
+  <link rel="prefetch" href="/assets/js/dist/services.bundle.js">
 
-            <link rel="next" href="/">
+  <link rel="next" href="/services">
 
-            <link rel="prefetch" href="/assets/js/dist/services.bundle.js">
+  <?php break;
+        case '/portfolio': ?>
 
-            <link rel="next" href="/services">
+  <link rel="preload" href="/assets/js/dist/portfolio.bundle.js" as="script">
 
-        <?php break; case '/services' : ?>
-        
-            <link rel="preload" href="/assets/js/dist/services.bundle.js" as="script"></script>
+  <link rel="prefetch" href="/assets/js/dist/about.bundle.js">
 
-            <link rel="prefetch" href="/assets/js/dist/about.bundle.js">
+  <link rel="next" href="/about">
 
-            <link rel="next" href="/about">
+  <link rel="prefetch" href="/assets/js/dist/home.bundle.js">
 
-            <link rel="prefetch" href="/assets/js/dist/home.bundle.js">
+  <link rel="next" href="/">
 
-            <link rel="next" href="/">
+  <link rel="prefetch" href="/assets/js/dist/services.bundle.js">
 
-            <link rel="prefetch" href="/assets/js/dist/portfolio.bundle.js">
+  <link rel="next" href="/services">
 
-            <link rel="next" href="/portfolio">
+  <?php break;
+        case '/services': ?>
 
-        <?php break; case '/contact' : ?>
-        
-            <!-- <link rel="preload" href="/assets/js/dist/home.bundle.js" as="script"></script> -->
+  <link rel="preload" href="/assets/js/dist/services.bundle.js" as="script">
+  </script>
 
-            <!-- <script async defer src=""></script> -->
+  <link rel="prefetch" href="/assets/js/dist/about.bundle.js">
 
-            <link type="text/css" rel="stylesheet" href="<?php echo $root_url . '/node_modules/bulma/css/bulma.css'; ?>">
+  <link rel="next" href="/about">
 
-        <?php break; ?>
+  <link rel="prefetch" href="/assets/js/dist/home.bundle.js">
 
-        <?php endswitch; ?>
+  <link rel="next" href="/">
+
+  <link rel="prefetch" href="/assets/js/dist/portfolio.bundle.js">
+
+  <link rel="next" href="/portfolio">
+
+  <?php break;
+        case '/contact': ?>
+
+  <!-- <link rel="preload" href="/assets/js/dist/home.bundle.js" as="script"></script> -->
+
+  <!-- <script async defer src=""></script> -->
+
+  <link type="text/css" rel="stylesheet"
+    href="<?php echo $root_url . '/node_modules/bulma/css/bulma.css'; ?>">
+
+  <?php break; ?>
+
+  <?php endswitch; ?>
 
 
-    <link type="text/css" rel="stylesheet" href="/assets/css/dist/bundle.css?ver=<?php echo date('h:i:s'); ?>">
+  <link type="text/css" rel="stylesheet"
+    href="/assets/css/dist/bundle.css?ver=<?php echo date('h:i:s'); ?>">
 
-    <link rel="preload" href="https://fonts.googleapis.com/css?family=Montserrat|Oswald&display=swap" as="style">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat|Oswald&display=swap" rel="stylesheet">
+  <link rel="preload" href="https://fonts.googleapis.com/css?family=Montserrat|Oswald&display=swap" as="style">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat|Oswald&display=swap" rel="stylesheet">
 
-    
 
-    <noscript>
-        <style>
-            /**
+
+  <noscript>
+    <style>
+      /**
             * Reinstate scrolling for non-JS clients
             */
-            .simplebar-content-wrapper {
-                overflow: auto;
-            }
-        </style>
-    </noscript>
+      .simplebar-content-wrapper {
+        overflow: auto;
+      }
+    </style>
+  </noscript>
 
 </head>
 
-<body>     
+<body>
 
-    <?php if ($request !== '/splash') : ?>
+  <?php if ($request !== '/splash') : ?>
 
-    <?php  loadMenu(); ?>
+  <?php  loadMenu(); ?>
 
-    <button class="edfx-toggle-button hamburger hamburger--elastic" type="button">
-        <span class="hamburger-box">
-            <span class="hamburger-inner"></span>
-        </span>
-    </button>
+  <button class="edfx-toggle-button hamburger hamburger--elastic" type="button">
+    <span class="hamburger-box">
+      <span class="hamburger-inner"></span>
+    </span>
+  </button>
 
-    <main class="site-container">
+  <main class="site-container">
 
     <?php endif; ?>
 
-     <!--[if lt IE 7]>
+    <!--[if lt IE 7]>
         <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
     <![endif]-->
 
-        <?php
-
-    
-
-        }
+    <?php
+    }
 }
